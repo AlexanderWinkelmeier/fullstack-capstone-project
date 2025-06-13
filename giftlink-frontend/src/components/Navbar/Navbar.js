76787190efecb5000e17cb5bed6c6127f7631d0c
Alexander Winkelmeier <r.a.winkelmeier@hotmail.de>
2025-06-13 07:31:22 -0400

    implemented SearchPage-Component and Style

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Kein Import von { Link } nötig, da wir nur <a>-Tags verwenden.
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -9,10 +10,14 @@ export default function Navbar() {
                 <ul className="navbar-nav">
                     {/* Task 1: Add links to Home and Gifts below */}
                     <li className="nav-item">
-                    <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
+                        <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
+                    </li>
+                    <li className="nav-item">
+                        {/* Korrigiert: von <Link> zu <a> geändert für Konsistenz */}
+                        <a className="nav-link" href="/app/search">Search</a>
                     </li>
                 </ul>
             </div>
